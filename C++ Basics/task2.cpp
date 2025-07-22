76a4f0a5f927f6d8ed4fcdc49c4e38f02927d00c
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2025-07-22 15:26:30 +0500

    starting commit

---
+++ C++ Basics/task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cin >> x >> y >> z;
+    int sum = x + y + z;
+    int avg = sum / 3;
+    cout << "Average: " << avg;
+    return 0;
+}
